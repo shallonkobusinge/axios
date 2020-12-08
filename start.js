d7b5c0647a96c298bd9f7bf5c99e6c655d7733f9
Kobsharon <kobshallon041@gmail.com>
2020-12-08 07:13:55 +0200

    changes

--- start.js
+++ start.js
@@ -90,8 +90,6 @@ document.getElementById('update').addEventListener('click', updateTodo);
 document.getElementById('delete').addEventListener('click', removeTodo);
 document.getElementById('sim').addEventListener('click', getData);
 document.getElementById('headers').addEventListener('click', customHeaders);
-document
-  .getElementById('transform')
-  .addEventListener('click', transformResponse);
+document.getElementById('transform').addEventListener('click', transformResponse);
 document.getElementById('error').addEventListener('click', errorHandling);
 document.getElementById('cancel').addEventListener('click', cancelToken);
